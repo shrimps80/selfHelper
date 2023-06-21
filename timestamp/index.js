76e8664404a454d1519539b338587912b5b1c93e
shrimp <shrimps80@gmail.com>
2023-06-21 09:29:00 +0800

    时间戳转换

--- timestamp/index.js
+++ timestamp/index.js
@@ -1,9 +1,14 @@
 new Vue({
     el     : '#pageContainer',
     data   : {
-        txtNowDate: (new Date()).toLocaleString(),
-        txtNowS   : Math.round((new Date()).getTime() / 1000),
-        curGMT    : (new Date()).getTimezoneOffset() / 60 * -1
+        formatter  : 'yyyy-MM-dd hh:mm:ss',
+        txtNowDate : (new Date()).toLocaleString(),
+        txtNowS    : Math.round((new Date()).getTime() / 1000),
+        curGMT     : (new Date()).getTimezoneOffset() / 60 * -1,
+        txtSrcStamp: '',
+        txtDesDate : '',
+        txtLocale  : '',
+        txtDesStamp: '',
     },
     mounted: function () {
         this.startTimestamp();
@@ -31,17 +36,41 @@ new Vue({
         },
 
         startTimestamp: function () {
-            let formatter     = 'yyyy-MM-dd hh:mm:ss';
             window.intervalId = window.setInterval(() => {
                 let localDate = new Date();
                 let gmtTime   = new Date(localDate.getTime() + localDate.getTimezoneOffset() * 60000);
                 let nowDate   = new Date(gmtTime.getTime() + this.curGMT * 60 * 60000);
 
-                this.txtNowDate = this.formatDate(nowDate, formatter);
+                this.txtNowDate = this.formatDate(nowDate, this.formatter);
                 this.txtNowS    = Math.round(nowDate.getTime() / 1000);
             }, 1000);
         },
 
+        //Unix时间戳 转 当地时间
+        stampToLocale() {
+            if (this.txtSrcStamp.length === 0) {
+                alert('请先填写你需要转换的Unix时间戳');
+                return;
+            }
+            if (!parseInt(this.txtSrcStamp, 10)) {
+                alert('请输入合法的Unix时间戳');
+                return;
+            }
+            let DesDate = new Date(parseInt(this.txtSrcStamp, 10) * 1000 + ((new Date()).getTimezoneOffset() + this.curGMT * 60) * 60000)
 
+            this.txtDesDate = this.formatDate(DesDate, this.formatter);
+        },
+
+        //当地时间 转 Unix时间戳
+        localeToStamp() {
+            if (this.txtLocale && !/\s\d+:\d+:\d+/.test(this.txtLocale)) {
+                this.txtLocale += ' 00:00:00';
+            }
+            let locale = (new Date(Date.parse(this.txtLocale) - ((new Date()).getTimezoneOffset() + this.curGMT * 60) * 60000)).getTime();
+            if (isNaN(locale)) {
+                alert('请输入合法的时间格式，如：2014-04-01 10:01:01，或：2014-01-01');
+            }
+            this.txtDesStamp = Math.round(locale / 1000);
+        }
     }
 })
