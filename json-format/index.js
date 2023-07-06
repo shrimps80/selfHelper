55f914b0027e35cccf2d9269a3a602bf82a12081
shrimp <shrimps80@gmail.com>
2023-07-06 16:33:59 +0800

    增加去转义和解码Unicode的功能

--- json-format/index.js
+++ json-format/index.js
@@ -6,7 +6,18 @@ new Vue({
     },
     methods: {
         format: function () {
-            this.placeHolder = JSON.stringify(JSON.parse(this.rawJson), null, 2)
+            this.placeHolder = this.formatJson(this.rawJson)
+        },
+        formatJson(json) {
+            // 去转义/
+            json = json.replace(/\\"/g, '"')
+
+            // 解码Unicode
+            json = json.replace(/\\u([\da-f]{4})/ig, function (match, p1) {
+                return String.fromCharCode(parseInt(p1, 16))
+            })
+
+            return JSON.stringify(JSON.parse(json), null, 2)
         }
     }
 })
