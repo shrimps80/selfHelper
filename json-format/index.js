cac4a3e79bf857590fb956c398c3bf8ecbed51f5
shrimp <shrimps80@gmail.com>
2023-07-06 16:31:54 +0800

    v1 json格式化

--- json-format/index.js
+++ json-format/index.js
@@ -1,5 +1,13 @@
 new Vue({
     el     : '#pageContainer',
-    data   : {},
-    methods: {}
+    data   : {
+        rawJson    : '',
+        placeHolder: '',
+    },
+    methods: {
+        format: function () {
+            this.placeHolder = JSON.stringify(JSON.parse(this.rawJson), null, 2)
+        }
+    }
 })
+
